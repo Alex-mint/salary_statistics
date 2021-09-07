a87265f95e44483ed942ad665915ba970cf8fd08
Alex <soyvitaly@gmail.com>
2021-09-07 09:27:07 +0200

    Убрал лишние преобразования типов

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def main():
         headers = [["Язык программирования", "Вакансий найдено",
                     "Вакансий обработано", "Средняя зарплата"]]
         title = f"{name} Moscow"
-        statistics_table = AsciiTable(tuple(headers + statistics), title)
+        statistics_table = AsciiTable(headers + statistics, title)
         print(statistics_table.table)
 
 
